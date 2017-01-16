ddd23aee850d0ea0da16a695444124f3570a73d3
witekbobrowski <witek@bobrowski.com.pl>
2017-01-16 12:22:51 +0100

    instant-messenger v.1.0.alpha-1

--- Java/Zestaw_7/src/chat/Client.java
+++ Java/Zestaw_7/src/chat/Client.java
@@ -12,17 +12,29 @@ public class Client{
     public static void main(String[] args) throws IOException{
             clientsID = new Integer(args[0]);
             recipientsID = new Integer(args[1]);
-            socket = new Socket("localhost", 9090);
+            if(serverOnlineCheck()){
+                socket = new Socket("localhost", 9090);
+            }else return;
             ClientReader reader = new ClientReader();
             reader.start();
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-            System.out.println("-------");
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
             String inputText;
             while((inputText = bufferedReader.readLine()) != null){
                 Message message = new Message(clientsID, recipientsID, inputText);
                 objectOutputStream.writeObject(message);
             }
+    }
+
+    public static boolean serverOnlineCheck() {
+        try (Socket socket = new Socket("localhost", 9090)) {
+            return true;
+        } catch (IOException ex) {
+            System.out.println("Unable to connect to the server. It may be caused by:");
+            System.out.println("1. Server is offline");
+            System.out.println("2. You are trying to connect to the wrong server, try other port or host");
+        }
+        return false;
     }
 
     static class ClientReader extends Thread {
@@ -32,7 +44,6 @@ public class Client{
 
         @Override
         public void run(){
-
             try {
                 Socket socket = Client.socket;
                 ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
--- Java/Zestaw_7/src/chat/Server.java
+++ Java/Zestaw_7/src/chat/Server.java
@@ -6,8 +6,8 @@ import java.net.*;
 public class Server{
 
     private static final int port = 9090;
-    private static final int maxClientsCount = 50;
-    private static final ClientSocket[] clientSockets = new ClientSocket[maxClientsCount];
+    private static final int clientsLimit = 50;
+    private static final ClientSocket[] clientSockets = new ClientSocket[clientsLimit];
 
     public static void main(String[] args) {
 
@@ -15,22 +15,23 @@ public class Server{
         Socket socket;
 
         try {
-            serverSocket = new ServerSocket(port, maxClientsCount);
+            serverSocket = new ServerSocket(port, clientsLimit);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        
         while (true) {
             try {
                 socket = serverSocket.accept();
                 int i = 0;
-                for (; i < maxClientsCount; i++) {
+                for (; i < clientsLimit; i++) {
                     if (clientSockets[i] == null) {
                         (clientSockets[i] = new ClientSocket(socket, clientSockets)).start();
                         System.out.println("Client have joined the server. Assigned ID: " + i);
                         break;
                     }
                 }
-                if (i == maxClientsCount) {
+                if (i == clientsLimit) {
                     ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
                     Message maxClientsReachedError = new Message(i, "Sorry, the server is currently full.");
                     objectOutputStream.writeObject(maxClientsReachedError);
@@ -43,5 +44,41 @@ public class Server{
         }
     }
 
+    static class ClientSocket extends Thread {
 
+        protected Socket socket;
+        private boolean connected;
+        private final ClientSocket[] clientSockets;
+        ObjectInputStream objectInputStream;
+        ObjectOutputStream objectOutputStream;
+
+        public ClientSocket(Socket clientSocket, ClientSocket[] clientSockets) {
+            this.socket = clientSocket;
+            this.clientSockets = clientSockets;
+            this.connected = true;
+        }
+
+        @Override
+        public void run() {
+            try {
+                objectInputStream = new ObjectInputStream(socket.getInputStream());
+                objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+                while(true) {
+                    while (objectInputStream.available() <= 0) {
+                        Message message = (Message) objectInputStream.readObject();
+                        System.out.print("\nSending message from client : " + message.sender + " to client : " + message.recipient);
+                        try {
+                            clientSockets[message.recipient].objectOutputStream.writeObject(message);
+                        } catch (NullPointerException e){
+                            System.out.print("| ERROR : message undelivered...");
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
