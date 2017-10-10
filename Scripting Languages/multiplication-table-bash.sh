3b898871ab9d85523af289f0e66fcb270c79102e
witekbobrowski <witek@bobrowski.com.pl>
2017-10-10 15:07:07 +0200

    SL: Add multiplication-table-bash.

---
+++ Scripting Languages/multiplication-table-bash.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Witold Bobrowski ; Group 2
+
+# FUNCTIONS
+
+# reduce: Take two parameters and concatenate arg1 with arg2 depending on arg2 value
+reduce() {
+  if (( $1 < 10 )); then
+    buffer="$buffer  $1"
+  else
+    buffer="$buffer $1"
+  fi
+}
+
+# print_multiplication_table: iterate over 1-9 digits and using 'reduce' accumulate output in 'buffer'
+print_multiplication_table() {
+  echo "* __1__2__3__4__5__6__7__8__9  "
+  for a in {1..9}; do
+    buffer=$a"|"
+    for b in {1..9}; do
+      reduce $(( $a * $b ))
+    done
+    echo "$buffer"
+  done
+}
+
+print_multiplication_table
--- Scripting Languages/whoami-bash.sh
+++ Scripting Languages/whoami-bash.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Witold Bobrowski
+# Witold Bobrowski ; Group 2
 
 # FUNCTIONS
 quiet() {
