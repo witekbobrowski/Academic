61a319c1007202c26ab2e929bd034860592fee4f
witekbobrowski <witek@bobrowski.com.pl>
2017-10-24 09:38:55 +0200

    SL: Updade ping-in-range (bash).

--- scripting-languages-winter-2017/ping-in-range.bash
+++ scripting-languages-winter-2017/ping-in-range.bash
@@ -9,7 +9,7 @@ address_a=(0 0 0 0)
 address_b=(0 0 0 0)
 current_address=(0 0 0 0)
 address_string=""
-
+number_of_adresses_to_ping=0
 # FUNCTIONS
 
 transform_address_to_string() {
@@ -33,22 +33,35 @@ transform_string_to_addresses() {
 	done
 }
 
+calculate_number_of_adresses_to_ping() {
+	local decimal_a=0
+	local decimal_b=0
+	for index in {0..3}; do
+		decimal_a=$(($decimal_a + $((${address_a[index]} * $((256 ** $((3 - $index))))))))
+		decimal_b=$(($decimal_b + $((${address_b[index]} * $((256 ** $((3 - $index))))))))
+	done
+	number_of_adresses_to_ping=$(($decimal_b - $decimal_a + 1))
+}
+
 ping_to_address() {
 	current_address=("${address_a[@]}")
-	for index in "3" "2" "1" "0"; do
-		while (( ${current_address[index]} < 255 )); do
-			transform_address_to_string
-			ping -c 1 "$address_string" > /dev/null
-    	if [ $? -eq 0 ]; then
-    		echo "node $address_string is up"
-    	else
-    		echo "node $address_string is down"
-    	fi
-			((++current_address[index]))
-			address_string=""
+	while (( $number_of_adresses_to_ping > 0 )); do
+		transform_address_to_string
+		echo $address_string
+		for index in {3..0}; do
+			if (( ${current_address[index]} < 255 )); then
+				((++current_address[$index]))
+				break
+			else
+				((current_address[$index]=0 ))
+				((++current_address[(($index - 1))]))
+			fi
 		done
+		address_string=""
+		((--number_of_adresses_to_ping))
 	done
 }
 
 transform_string_to_addresses $1 $2
+calculate_number_of_adresses_to_ping
 ping_to_address
