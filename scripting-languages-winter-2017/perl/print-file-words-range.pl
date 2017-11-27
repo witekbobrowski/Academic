f01d35921e65f7e52218a06fca2b6eb41898d015
witekbobrowski <witek@bobrowski.com.pl>
2017-11-27 20:11:06 +0100

    SL: Add print-file-words-range (perl).

---
+++ scripting-languages-winter-2017/perl/print-file-words-range.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl
+# Witold Bobrowski ; Group 2
+#
+# Print words in range
+
+use strict;
+use warnings;
+
+# VARIABLES
+
+my $left_bound = 0;
+my $right_bound = 0;
+my @range = ();
+
+# SUBROUTINES
+
+sub print_error_and_exit {
+	print "Usage: perl print-file-words-range.pl [lower bound] [upper bound] [file name] [...]\n";
+	exit;
+}
+
+sub is_numeric_and_positive {
+	return ($_[0] =~ /[0-9]+$/) && ($_[0] !~ /^[-]/)
+}
+
+sub get_range {
+	$left_bound = $_[0];
+	$right_bound = $_[0];
+	if ($left_bound < $right_bound) {
+		return ($left_bound .. $right_bound);
+	} else {
+		return reverse ($right_bound .. $left_bound);
+	}
+}
+
+sub range_is_valid {
+	return (($left_bound < $_[0] && $right_bound <= $_[0]) || ($right_bound < $_[0] && $left_bound <= $_[0]))
+}
+
+sub print_file {
+    open(my $data, '<:encoding(UTF-8)', $_[0])
+        or die "Could not open file '$_[0]' $!";
+    while (my $line = <$data>) {
+			my @words = split(" ",$line);
+			chomp $line;
+			if (range_is_valid($#words)) {
+				foreach my $word (@range) {
+					print "$words[$word] ";
+				}
+			}
+			print "\n";
+    }
+}
+
+# Main program
+
+if ($#ARGV < 2) {
+	print_error_and_exit;
+}
+
+foreach my $argument (0 .. 1) {
+  if (!is_numeric_and_positive($ARGV[$argument])) {
+		print_error_and_exit;
+	}
+}
+
+@range = get_range($ARGV[0], $ARGV[1]);
+
+foreach my $argument (2 .. $#ARGV) {
+  print_file($ARGV[$argument]);
+}
