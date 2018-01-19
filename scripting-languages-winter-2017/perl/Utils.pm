f54333c1305756c948b19ad0db08c3a623875773
witekbobrowski <witek@bobrowski.com.pl>
2018-01-19 14:18:53 +0100

    SL: Update Utils module with get_grade function (perl).

--- scripting-languages-winter-2017/perl/Utils.pm
+++ scripting-languages-winter-2017/perl/Utils.pm
@@ -25,6 +25,35 @@ sub looks_like_number {
     return "$_[0]" =~ /^([+-]?)(?=\d|\.\d)\d*(\.\d*)?([Ee]([+-]?\d+))?$/;
 }
 
+# get grade from string
+sub get_grade {
+    my $possible_grade = "$_[0]";
+    my $floating_point = 0;
+    my $number         = 0;
+    if ( "$possible_grade" =~ /^\d*(\.\d*)?$/ ) {
+        $number = $possible_grade;
+    }
+    elsif ( "$possible_grade" =~ /^\d[-]?$/ ) {
+        $floating_point = -0.25;
+        chop($possible_grade);
+        $number = $possible_grade;
+    }
+    elsif ( "$possible_grade" =~ /^[-]?\d$/ ) {
+        $floating_point = -0.25;
+        $number = substr( "$possible_grade", 1 );
+    }
+    elsif ( "$possible_grade" =~ /^\d[+]?$/ ) {
+        $floating_point = +0.25;
+        chop($possible_grade);
+        $number = $possible_grade;
+    }
+    elsif ( "$possible_grade" =~ /^[+]?\d$/ ) {
+        $floating_point = +0.25;
+        $number = substr( "$possible_grade", 1 );
+    }
+    return ( 2 <= $number + $floating_point && $number + $floating_point <= 5 ) ? $number + $floating_point : 0;
+}
+
 # regexp's from https://docstore.mik.ua/orelly/perl4/cook/ch02_02.htm
 
 # backward compatibility
