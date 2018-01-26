121458f58e94ad47be4740bc8889aee1fcdfbb5a
witekbobrowski <witek@bobrowski.com.pl>
2018-01-26 20:06:37 +0100

    SL: Update and beautify whoami (bash).

--- scripting-languages-winter-2017/shell/whoami.bash
+++ scripting-languages-winter-2017/shell/whoami.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # Witold Bobrowski ; Group 2
+#
+# Display current user
 
 # VARIABLES
 # error variable which will be returned on exit.
@@ -9,48 +11,48 @@ program_should_exit="false"
 
 # FUNCTIONS
 quiet() {
-	exit $error
+    exit $error
 }
 
 display_help() {
-  echo "whoami.tcsh -- print current user"
-  echo
-	echo "Usage: ./whoami.bash [--help] [-h] [--quiet] [-q]"
-	echo
-	echo "Options:"
-	echo "	-h | --help 	Show help"
-	echo "	-q | --quiet 	exit"
-	quiet
+    echo "whoami.tcsh -- print current user"
+    echo
+    echo "Usage: ./whoami.bash [--help] [-h] [--quiet] [-q]"
+    echo
+    echo "Options:"
+    echo "	-h | --help 	Show help"
+    echo "	-q | --quiet 	exit"
+    quiet
 }
 
 display_user() {
-	whoami
+    whoami
 }
 
 error() {
-	error=1
-	if [[ $1 != "" ]]; then
-		echo -e "[!] Unknown command: \`$1\`"
-		echo
-		display_help
-	fi
+    error=1
+    if [[ $1 != "" ]]; then
+        echo -e "[!] Unknown command: \`$1\`"
+        echo
+        display_help
+    fi
 }
 
 # Main program
 
 while [[ $# -gt 0 ]]; do
-  case "$1" in
-    -h|--help) display_help ;;
-  	-q|--quiet)
-			program_should_exit="true"
-			shift
-			;;
-		-*) error $1 ;;
-		*) shift ;;
-  esac
+    case "$1" in
+        -h|--help) display_help ;;
+        -q|--quiet)
+            program_should_exit="true"
+            shift
+            ;;
+        -*) error $1 ;;
+        *) shift ;;
+    esac
 done
 
 if [[ $program_should_exit != "true" ]]; then
-	display_user
+    display_user
 fi
 quiet
