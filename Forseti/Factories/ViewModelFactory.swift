b1736128ba46e9bdfca54125ce8bd013b8c0cc3e
Witek Bobrowski <witek@bobrowski.com.pl>
2018-04-20 14:01:47 +0200

    Add ViewModelFactory.

---
+++ Forseti/Factories/ViewModelFactory.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModelFactory.swift
+//  Forseti
+//
+//  Created by Witek Bobrowski on 20/04/2018.
+//  Copyright © 2018 Witek Bobrowski. All rights reserved.
+//
+
+import Foundation
+
+protocol ViewModelFactory {
+    var reviewViewModel: ReviewViewModel { get }
+}
