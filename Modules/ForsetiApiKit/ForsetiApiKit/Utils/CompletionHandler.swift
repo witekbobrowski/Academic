21ac8f604b55220615374ce36b37f36e55873f13
Witek Bobrowski <witek@bobrowski.com.pl>
2018-05-09 23:25:22 +0200

    Add CompletionHandler type to ApiKit.

---
+++ Modules/ForsetiApiKit/ForsetiApiKit/Utils/CompletionHandler.swift
@@ -0,0 +1,12 @@
+//
+//  CompletionHandler.swift
+//  ForsetiApiKit
+//
+//  Created by Witek Bobrowski on 09/05/2018.
+//  Copyright © 2018 Witek Bobrowski. All rights reserved.
+//
+
+import Foundation
+
+typealias CompletionHandler<T> = (Result<T>) -> Void
+
