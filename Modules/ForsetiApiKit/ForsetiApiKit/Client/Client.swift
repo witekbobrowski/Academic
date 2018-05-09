2f6f0b7aaa7a6a5e1a10a873fee901e5b151a2b4
Witek Bobrowski <witek@bobrowski.com.pl>
2018-05-09 23:42:26 +0200

    Add ServiceFactory to ApiKit.

--- Modules/ForsetiApiKit/ForsetiApiKit/Client/Client.swift
+++ Modules/ForsetiApiKit/ForsetiApiKit/Client/Client.swift
@@ -10,23 +10,21 @@ import Foundation
 
 public protocol Client {
     var authenticationService: AuthenticationService { get }
-    var userService: Any { get }
-    var accountService: Any { get }
 }
 
 class ForsetiClient: Client {
 
     private let restClient: RestClient
-    private let dependencyContainer: Any
+    private let dependencyContainer: DependencyContainer
 
     init(restClient: RestClient,
-         dependencyContainer: Any) {
+         dependencyContainer: DependencyContainer) {
         self.restClient = restClient
         self.dependencyContainer = dependencyContainer
     }
 
-    var authenticationService: AuthenticationService { return AuthenticationServiceImplementation(restClient: restClient) }
-    var userService: Any { return dependencyContainer }
-    var accountService: Any { return dependencyContainer }
+    var authenticationService: AuthenticationService {
+        return dependencyContainer.authenticationService(restClient: restClient)
+    }
 
 }
