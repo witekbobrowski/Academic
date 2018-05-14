57d035b42801394cdcf9e19c7b6da62f9310a635
Witek Bobrowski <witek@bobrowski.com.pl>
2018-05-14 22:32:46 +0200

    Add ProfileCoordinatorModel protocol.

---
+++ Forseti/Scenarios/Profile/ProfileCoordinatorModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileCoordinatorModel.swift
+//  Forseti
+//
+//  Created by Witek Bobrowski on 14/05/2018.
+//  Copyright © 2018 Witek Bobrowski. All rights reserved.
+//
+
+import Foundation
+
+protocol ProfileCoordinatorModel {
+    var profileViewController: ProfileViewController { get }
+}
