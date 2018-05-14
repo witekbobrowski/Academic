67e055f215c2dd4c29162af65920b8f44a9e4cfc
Witek Bobrowski <witek@bobrowski.com.pl>
2018-05-14 22:34:27 +0200

    Add ProfileCoordinatorModelImplementation class.

--- Forseti/Scenarios/Profile/ProfileCoordinatorModel.swift
+++ Forseti/Scenarios/Profile/ProfileCoordinatorModel.swift
@@ -7,7 +7,36 @@
 //
 
 import Foundation
+import ForsetiApiKit
 
 protocol ProfileCoordinatorModel {
     var profileViewController: ProfileViewController { get }
+}
+
+class ProfileCoordinatorModelImplementation: ProfileCoordinatorModel {
+
+    private let dependencyContainer: DependencyContainer
+    private let client: Client
+
+    var profileViewController: ProfileViewController {
+        return configuredProfileViewController()
+    }
+
+    init(client: Client,
+         dependencyContainer: DependencyContainer) {
+        self.client = client
+        self.dependencyContainer = dependencyContainer
+    }
+
+}
+
+extension ProfileCoordinatorModelImplementation {
+
+    func configuredProfileViewController() -> ProfileViewController {
+        let viewModel = dependencyContainer.profileViewModel(userService: client.userService)
+        let viewController = dependencyContainer.profileViewController
+        viewController.viewModel = viewModel
+        return viewController
+    }
+
 }
