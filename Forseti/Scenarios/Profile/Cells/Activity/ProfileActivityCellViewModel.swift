61309591969b6be061fc4964948561d31e4c65fc
Witek Bobrowski <witek@bobrowski.com.pl>
2018-05-15 20:36:57 +0200

    Add ProfileActivityCellViewModel protocol.

---
+++ Forseti/Scenarios/Profile/Cells/Activity/ProfileActivityCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileActivityCellViewModel.swift
+//  Forseti
+//
+//  Created by Witek Bobrowski on 15/05/2018.
+//  Copyright © 2018 Witek Bobrowski. All rights reserved.
+//
+
+import Foundation
+
+protocol ProfileActivityCellViewModel {
+    var title: String { get }
+}
