f311c4a84da7724ee171010d5c8b81c39a808c2f
Witek Bobrowski <witek@bobrowski.com.pl>
2018-05-17 15:06:34 +0200

    Add AccountNumber struct.

---
+++ Modules/ForsetiApiKit/ForsetiApiKit/Model/AccountNumber.swift
@@ -0,0 +1,18 @@
+//
+//  AccountNumber.swift
+//  ForsetiApiKit
+//
+//  Created by Witek Bobrowski on 17/05/2018.
+//  Copyright © 2018 Witek Bobrowski. All rights reserved.
+//
+
+import Foundation
+
+public struct AccountNumber {
+    let accountNumber: String
+    let comments: [String: Comment]
+    let removedUsersComments: [String: Comment]
+    let thumbsDetails: [String: ThumbsDetails]
+    let thumbsDown: Int
+    let thumbsUp: Int
+}
