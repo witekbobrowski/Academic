cb0c369920c0a65963d9c3844d68d99c361b04dd
Witek Bobrowski <witek@bobrowski.com.pl>
2018-05-17 20:00:56 +0200

    Add AccountNumberDetailsTableViewCell.

--- Forseti/Scenarios/AccountNumber/Cells/Details/AccountNumberDetailsCellViewModel.swift
+++ Forseti/Scenarios/AccountNumber/Cells/Details/AccountNumberDetailsCellViewModel.swift
@@ -10,14 +10,18 @@ import Foundation
 import ForsetiApiKit
 
 protocol AccountNumberDetailsCellViewModel {
+    var bank: String? { get }
     var number: String { get }
+    var location: String? { get }
 }
 
 class AccountNumberDetailsCellViewModelImplementation: AccountNumberDetailsCellViewModel {
 
     private let accountNumber: AccountNumber
 
+    var bank: String? { return nil }
     var number: String { return accountNumber.accountNumber }
+    var location: String? { return nil }
 
     init(accountNumber: AccountNumber) {
         self.accountNumber = accountNumber
---
+++ Forseti/Scenarios/AccountNumber/Cells/Details/AccountNumberDetailsTableViewCell.swift
@@ -0,0 +1,49 @@
+//
+//  AccountNumberDetailsTableViewCell.swift
+//  Forseti
+//
+//  Created by Witek Bobrowski on 17/05/2018.
+//  Copyright © 2018 Witek Bobrowski. All rights reserved.
+//
+
+import UIKit
+
+class AccountNumberDetailsTableViewCell: UITableViewCell {
+
+    @IBOutlet private weak var cardView: UIView!
+    @IBOutlet private weak var bankLabel: UILabel!
+    @IBOutlet private weak var numberLabel: UILabel!
+    @IBOutlet private weak var locationLabel: UILabel!
+
+    var viewModel: AccountNumberDetailsCellViewModel? { didSet { update(with: viewModel)  } }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+}
+
+extension AccountNumberDetailsTableViewCell {
+
+    private func setupView() {
+        backgroundColor = .clear
+
+    }
+
+    private func setupCardView() {
+        cardView.layer.cornerRadius = 8
+        cardView.layer.shadowColor = UIColor.lightGray.cgColor
+        cardView.layer.shadowRadius = 4
+        cardView.layer.shadowOffset = .zero
+        cardView.layer.shadowOpacity = 0.5
+        cardView.backgroundColor = .lightGray
+    }
+
+    private func update(with viewModel: AccountNumberDetailsCellViewModel?) {
+        bankLabel.text = viewModel?.bank
+        numberLabel.text = viewModel?.number
+        locationLabel.text = viewModel?.location
+    }
+
+}
